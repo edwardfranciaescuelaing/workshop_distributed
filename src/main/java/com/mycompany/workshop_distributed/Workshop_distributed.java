c1d96fecfe62b062d0c17de8fe806b704f8b012e
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-10-26 08:57:47 -0500

    first commit

---
+++ src/main/java/com/mycompany/workshop_distributed/Workshop_distributed.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.workshop_distributed;
+
+/**
+ *
+ * @author lab
+ */
+public class Workshop_distributed {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
